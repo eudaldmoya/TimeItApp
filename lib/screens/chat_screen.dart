8f85318139dfd94effc5437a8f862ce8436b1de3
seaanmarina <104677546+seaanmarina@users.noreply.github.com>
2023-01-07 17:01:19 +0100

    lista de grupos
    no es util para nuestra aplicacion, es solo para coger practica

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -7,7 +8,34 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Firebase WhatsApp")),
-      body: Center(child: Text("Group Page")),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('/Company/kGCOpHgRyiIYLr4Fwuys/Chat')
+            .snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.hasError) {
+            return ErrorWidget(snapshot.error.toString());
+          }
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final qsnap = snapshot.data!;
+          final docs = qsnap.docs;
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              final doc = docs[index];
+              return ListTile(
+                title: Text(doc['name']),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
