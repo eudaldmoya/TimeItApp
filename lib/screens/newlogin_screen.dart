c8e1aaed2db80bf3a44417a7c16c8132d684ad08
seaanmarina <104677546+seaanmarina@users.noreply.github.com>
2023-01-08 18:23:08 +0100

    creacion de log in ui

--- lib/screens/newlogin_screen.dart
+++ lib/screens/newlogin_screen.dart
@@ -15,15 +15,115 @@ class _LoginPageState extends State<LoginPage> {
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
+        body: Column(
+      children: [
+        Container(
+          width: w,
+          height: h * 0.3,
+          decoration: BoxDecoration(color: Colors.pink),
+        ),
+        Container(
+            margin: EdgeInsets.only(left: 20, right: 20),
             width: w,
-            height: h*0.3,
-            decoration: BoxDecoration(color: Colors.pink),
-          )
-        ],
-      )
-    );
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Hello",
+                  style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Sign into your account",
+                  style: TextStyle(fontSize: 20, color: Colors.grey[500]),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        spreadRadius: 7,
+                        blurRadius: 10,
+                        offset: Offset(1, 1),
+                        color: Colors.grey.withOpacity(0.2),
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        spreadRadius: 7,
+                        blurRadius: 10,
+                        offset: Offset(1, 1),
+                        color: Colors.grey.withOpacity(0.2),
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                  ),
+                )
+              ],
+            )),
+        SizedBox(
+          height: 70,
+        ),
+        Container(
+          width: w * 0.5,
+          height: h * 0.08,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.pink,
+          ),
+          child: Center(
+            child: Text(
+              "Sign in",
+              style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+        ),
+     
+      ],
+    ));
   }
 }
